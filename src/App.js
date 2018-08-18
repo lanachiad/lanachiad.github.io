fa68196a5cf6205273be852830f3edb310e8c455
Lana Chiad <lanachiad7@gmail.com>
2018-08-18 12:49:43 -0400

    Create nav buttons for each module

--- src/App.js
+++ src/App.js
@@ -7,7 +7,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Portfolio lol</h1>
+        <h1>Lana Chiad</h1>
+        <nav>
+          <ul>
+            <li>
+              <button id="about-btn">About</button>
+            </li>
+            <li>
+              <button id="work-btn">Work</button>
+            </li>
+            <li>
+              <button id="resume-btn">Resume</button>
+            </li>
+          </ul>
+        </nav>
         <About />
         <Work />
       </div>
