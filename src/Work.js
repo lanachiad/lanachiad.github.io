0f9943bad5239a7c0a48f73cb79181e1f3ad8efb
Lana Chiad <lanachiad7@gmail.com>
2018-08-18 11:57:00 -0400

    Add default

---
+++ src/Work.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Work extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Work test</h2>
+      </div>
+    );
+  }
+}
+
+export default Work;
