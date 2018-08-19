fdf635466a430717eb8122caad8859ffae0066cd
Lana Chiad <lanachiad7@gmail.com>
2018-08-19 14:52:52 -0400

    Add logic for rendering one component at a time

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 import About from './About';
 import Work from './Work';
+import Resume from './Resume';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAbout: false,
+      showWork: false,
+      showResume: false
+    };
+  }
+
+  onAboutBtnClick = () => {
+    this.onlyOneTrue();
+    this.setState({ showAbout: !this.state.showAbout });
+  };
+
+  onWorkBtnClick = () => {
+    this.onlyOneTrue();
+    this.setState({ showWork: !this.state.showWork });
+  };
+
+  onResumeBtnClick = () => {
+    this.onlyOneTrue();
+    this.setState({ showResume: !this.state.showResume });
+  };
+
+  onlyOneTrue = () => {
+    if (this.state.showAbout === true) {
+      this.setState({ showAbout: false });
+    } else if (this.state.showResume === true) {
+      this.setState({ showResume: false });
+    } else if (this.state.showWork === true) {
+      this.setState({ showWork: false });
+    }
+  };
+
   render() {
     return (
       <div className="App">
@@ -11,18 +46,25 @@ class App extends Component {
         <nav>
           <ul>
             <li>
-              <button id="about-btn">About</button>
+              <button id="about-btn" onClick={this.onAboutBtnClick}>
+                About
+              </button>
             </li>
             <li>
-              <button id="work-btn">Work</button>
+              <button id="work-btn" onClick={this.onWorkBtnClick}>
+                Work
+              </button>
             </li>
             <li>
-              <button id="resume-btn">Resume</button>
+              <button id="resume-btn" onClick={this.onResumeBtnClick}>
+                Resume
+              </button>
             </li>
           </ul>
         </nav>
-        <About />
-        <Work />
+        {this.state.showAbout ? <About /> : null}
+        {this.state.showWork ? <Work /> : null}
+        {this.state.showResume ? <Resume /> : null}
       </div>
     );
   }
